872f9e1c76531dda8c5262846c729074eecda79c
Alex <kryptoholyx@googlemail.com>
2019-06-10 00:04:12 +0200

    MapManager and testMap

--- server/world/MapManager.js
+++ server/world/MapManager.js
@@ -1,8 +1,47 @@
+const fs = require('fs');
 var MapManager = new class {
 	constructor() {
-		this._setup();
+		let self = this;
+		this._loadedMaps = [];
+		this.loadMaps();
 	}
-	_setup() {
-		
+	get maps() {
+		return this._loadedMaps.map(e => e.name);
+	}
+	getMapData(name) {
+		let map = this._loadedMaps.find(e => {
+			return e.name == name;
+		})
+		if (map) {
+			return {
+				spawns: map.spawnpoints || [],
+				objects: map.objects || [];
+			}
+		}
+	}
+	isMapLoaded(fileName) {
+		return this._loadedMaps[fileName.replace(".js", "")] != undefined;
+	}
+	loadMap(fileName) {
+		let self = this;
+		console.log("loadmap", fileName);
+		let temp_map = require("./maps/" + fileName);
+		self._loadedMaps[fileName.replace(".js", "")] = {
+			name: temp_map.name,
+			objects: temp_map.objects
+		}
+		console.log("Loaded Map", temp_map.name)
+	}
+	loadMaps() {
+		let self = this;
+		fs.readdir("./maps", function(err, files) {
+			files.forEach(function(map) {
+				if (map.indexOf(".js") > -1) {
+					if (self.isMapLoaded(map) == false) {
+						self.loadMap(map);
+					}
+				}
+			})
+		});
 	}
 }
