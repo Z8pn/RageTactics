2d8ed594014323b8be0a498fb5d6bb50492f0b4e
Alex <kryptoholyx@googlemail.com>
2019-07-20 07:40:50 +0200

    MapLoader caching and reloading in runtime

--- server/world/MapManager.js
+++ server/world/MapManager.js
@@ -2,24 +2,43 @@ const fs = require('fs');
 var MapManager = class {
 	constructor() {
 		let self = this;
+		console.log("Init Map Loader!")
 		this._loadedMaps = [];
 		this.loadMaps();
 	}
 	get maps() {
-		return this._loadedMaps.map(e => e.name);
+		let self = this;
+		let m = Object.keys(self._loadedMaps).map(e => {
+			return self._loadedMaps[e].name;
+		});
+		console.log(m);
+		/*console.log("maps",Object.keys(self._loadedMaps).map(e => {
+			let e = self._loadedMaps[e];
+			return e.name;
+		}))*/
+
+		return m;
 	}
 	getMapData(name) {
-		let map = this._loadedMaps.find(e => {
+		let self = this;
+
+		let map = Object.keys(this._loadedMaps).findIndex(e => {
+			e = self._loadedMaps[e];
 			return e.name == name;
 		})
+		map = this._loadedMaps[Object.keys(this._loadedMaps)[map]];
 		if (map) {
 			return {
-				spawns: map.spawnpoints || [],
+				name:map.name || "",
+				image: map.image || "",
+				max_players: map.max_players || 10,
+				spawns: map.spawns || [],
 				objects: map.objects || [],
+				previewCam: map.previewCam || {},
 				teams: map.teams || [{
 					name: "Team 1",
 					clothing: []
-				},{
+				}, {
 					name: "Team 2",
 					clothing: []
 				}]
@@ -35,18 +54,30 @@ var MapManager = class {
 		let self = this;
 		console.log("loadmap", fileName);
 		let temp_map = require("./maps/" + fileName);
+		console.log("temp_map",temp_map);
 		self._loadedMaps[fileName.replace(".js", "")] = {
 			name: temp_map.name,
-			objects: temp_map.objects
+			objects: temp_map.objects,
+			spawns: temp_map.spawns,
+			teams: temp_map.teams,
+			name: temp_map.name,
+			image: temp_map.image,
+			previewCam: temp_map.previewCam,
+			max_players: temp_map.max_players
 		}
 		console.log("Loaded Map", temp_map.name)
 	}
 	loadMaps() {
 		let self = this;
-		fs.readdir("./maps", function(err, files) {
+		console.log("load dir");
+		fs.readdir("./packages/RageTactics/world/maps", function(err, files) {
+				console.log("err..",err);
+				console.log("files..",files);
 			files.forEach(function(map) {
+				console.log("loading..");
 				if (map.indexOf(".js") > -1) {
 					if (self.isMapLoaded(map) == false) {
+						console.log(map);
 						self.loadMap(map);
 					}
 				}
