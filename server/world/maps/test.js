37f70d1ba84b830e4c3a53f0437c454f29f868cc
Alex <kryptoholyx@googlemail.com>
2019-07-20 07:40:55 +0200

    testmap

--- server/world/maps/test.js
+++ server/world/maps/test.js
@@ -1,12 +1,24 @@
 module.exports = {
 	name: "testMap",
 	objects: [],
+	spawns: [{x:0,y:0,z:1000,rx:0,ry:0,rz:0}],
+	max_players: 16,
+	image: "https://cdn.discordapp.com/attachments/597607496357642250/601864455269384239/Grand_Theft_Auto_V_Screenshot_2019.07.19_-_21.45.51.19.png",
 	teams: [{
-		name: "Attackers",
-		clothing: []
+		name: "Team #1",
+		clothing: [],
+		max:8
 	}, {
-		name: "Defenders",
-		clothing: []
+		name: "Team #2",
+		clothing: [],
+		max:8
 	}],
-	spawns
+	previewCam:{
+		x:100,
+		y:100,
+		z:100,
+		px:200,
+		py:200,
+		pz:10
+	}
 }
