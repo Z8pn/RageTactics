89151042177f0b4d41965ba3fb439b8584b362fb
Alex <kryptoholyx@googlemail.com>
2019-07-24 08:24:21 +0200

    HUB:PlayerCam

--- client/development/hub.js
+++ client/development/hub.js
@@ -39,3 +39,34 @@ mp.events.add("HUB:LoadData", (safeZones, weapons, vehicles) => {
 	vehicles = JSON.parse(vehicles);
 	HUB.loadData(safeZones, weapons, vehicles);
 });
+mp.events.add("HUB:PlayerCam", () => {
+	mp.players.local.freezePosition(true);
+	let cur_x = mp.defaultCam.getCoord().x;
+	let cur_y = mp.defaultCam.getCoord().y;
+	let cur_z = mp.defaultCam.getCoord().z;
+	let camera2 = mp.cameras.new('default', new mp.Vector3(cur_x, cur_y, cur_z + 100), new mp.Vector3(), 60);
+	camera2.pointAtCoord(cur_x, cur_y, cur_z);
+	camera2.setActive(true);
+	mp.defaultCam = camera2;
+	mp.game.cam.renderScriptCams(true, false, 0, true, false);
+	//
+	setTimeout(function() {
+		let camera3 = mp.cameras.new('default', new mp.Vector3(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z + 300), new mp.Vector3(), 60);
+		camera3.pointAtCoord(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z);
+		camera3.setActiveWithInterp(mp.defaultCam.handle, 1000, 1, 1);
+		mp.defaultCam = camera3;
+		setTimeout(function() {
+			let camera4 = mp.cameras.new('default', new mp.Vector3(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z + 1), new mp.Vector3(), 60);
+			camera4.pointAtCoord(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z);
+			mp.game.cam.doScreenFadeOut(3500);
+			camera4.setActiveWithInterp(mp.defaultCam.handle, 3500, 1, 1);
+			mp.defaultCam = camera4;
+			setTimeout(function() {
+				mp.game.cam.renderScriptCams(false, false, 0, true, false);
+				setTimeout(function() {
+					mp.game.cam.doScreenFadeIn(500);
+				}, 200)
+			}, 3500);
+		}, 1100)
+	}, 100)
+});
